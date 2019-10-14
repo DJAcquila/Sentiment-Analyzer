29c7a6b4e38dc428e52f58ebba2afc7c01d6c9f4
Acquila Santos Rocha <acquila.santos@gmail.com>
2019-10-14 19:07:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,40 @@ import sys
 import pickle
 from Modules.SentAnalyzer.sentiment import SentAnalyzer
 import speech_recognition as sr 
-
+import pyaudio
 import os
 
 def main():
     a = SetPersistentClassifier()
     print("Welcome to SentAnalyzer\nDescribe your feelings to me, and I will tell you if it's bad or good")
     r = sr.Recognizer()
+
     with sr.Microphone() as source:
         try:
             while True :
-                audio = r.listen(source)
+                audio = r.listen(source,timeout=3, phrase_time_limit=3)
                 try:
                     sentence = r.recognize_google(audio)
                     print('You said: {}'.format(sentence))
                     if sentence.strip().lower() in ['exit', 'quit']:
                         break
 
-                    sentiment = a.analyze(sentence)
-                    if sentiment == 'pos':
-                        print("\tThis is good")
-                    elif sentiment == 'neg':
-                        print ("\tThis is bad")
+                    if sentence.strip().lower() not in ["hello", "hi", "what's up", "hey", "what's your name", "your name", "name", "how are you"]:
+                        sentiment = a.analyze(sentence)
+                        if sentiment == 'pos':
+                            print("Psychorobot: This is good\n")
+                        elif sentiment == 'neg':
+                            print("Psychorobot: This is bad\n")
+
+                    elif sentence.strip().lower() in ["hello", "hi", "what's up", "hey"]:
+                        print("Psychorobot: Hey!\n")
+
+                    elif sentence.strip().lower() in ["what's your name", "your name", "name"]:
+                        print("Psychorobot: My name is Psychorobot\n")
+
+                    elif sentence.strip().lower() in ["how are you"]:
+                        print("Psychorobot: I am angry and tired\n")
+
                  
                 except Exception as e:
                     pass
